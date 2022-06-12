a81aee7615f4c6215a59541b77829d272cdd9d1d
pvianap <pedro.v.parente@gmail.com>
2022-06-12 22:13:09 +0200

    TODO 1 ajouter la fonctionnalité au bouton(x)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,29 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+const closeBtn = document.querySelectorAll('.close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+
+//close model event
+closeBtn.forEach((btn) => btn.addEventListener('click', closeModal));
+
+function closeModal() {
+  modalbg.style.display = 'none';
+}
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
-
-
