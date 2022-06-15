f5c0fa9416cac065fde1bce1f7aebc98bd279ba6
pvianap <pedro.v.parente@gmail.com>
2022-06-15 11:41:54 +0200

    Begin of validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,6 +29,45 @@ function launchModal() {
 }
 
 // prevent submit
-form.addEventListener('submit', (e) => {
+modalbg.addEventListener('submit', (e) => {
   e.preventDefault();
+});
+
+// Disable Submit
+function disableSubmit(disabled) {
+  if (disabled) {
+    document.getElementById('submit-btn').setAttribute('disabled', true);
+  } else {
+    document.getElementById('submit-btn').removeAttribute('disabled');
+  }
+}
+
+// Input values
+
+let fistName = document.getElementById('first').value;
+
+// Regex
+let firstNameX = /^[a-z ,.'-]{2,60}$/i;
+let lastNameX = /^[a-z ,.'-]{2,60}$/i;
+let emailX =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+let quantityX = /^[0-9]+$/;
+
+// // Validations
+
+// var validations =[fistName, lastName, email, nbConcours, radioBtn, conditionG];
+// function getCodeValidation() {
+//   return document.getElementById("code-validation");
+// }
+
+document.getElementById('code').addEventListener('input', function (e) {
+  if (
+    firstNameX.test(e.target.value) &&
+    ) {
+    getCodeValidation().innerText = 'Code valide';
+    disableSubmit(false);
+  } else {
+    getCodeValidation().innerText = 'Code invalide';
+    disableSubmit(true);
+  }
 });
