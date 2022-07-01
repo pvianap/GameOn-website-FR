d576e134c54fc6be4317d9239342688c247838f9
pvianap <pedro.v.parente@gmail.com>
2022-07-01 13:13:14 +0200

    Finished all validations with messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -41,7 +41,9 @@ function launchModal() {
 // prevent submit
 modalbg.addEventListener('submit', (e) => {
   e.preventDefault();
+  isValid = true;
   validateAll();
+  console.log(isValid);
 });
 
 // Disable Submit
@@ -59,81 +61,178 @@ function disableSubmit(disabled) {
 // Regex
 let firstX = /^[a-z ,.'-]{2,60}$/i;
 let lastX = /^[a-z ,.'-]{2,60}$/i;
-let emailX =
-  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+let emailX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
 let quantityX = /^[0-9]+$/;
-let birthdateX =
-  /^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$/;
 
 // VALIDATIONS
+var isValid = true;
 function validateFirst() {
-  firstX.test(
-    first.value
-      ? (first.style.border = 'solid green')
-      : (first.style.border = 'solid red')
-  );
+  if (!firstX.test(first.value)) {
+    first.style.border = 'solid red';
+    isValid = false;
+    var firstAlert = document.createElement('p');
+    firstAlert.setAttribute('id', 'firstAlert');
+    var firstLabel = document.querySelector('label[for="first"]');
+    firstAlert.textContent = errorMessages['first'];
+    if (firstLabel.firstElementChild == null) {
+      firstLabel.appendChild(firstAlert);
+    }
+  } else {
+    first.style.border = 'solid green';
+    if (document.contains(document.getElementById('firstAlert'))) {
+      document.getElementById('firstAlert').remove();
+    }
+  }
 }
 
 function validateLast() {
-  lastX.test(
-    last.value
-      ? (last.style.border = 'solid green')
-      : (last.style.border = 'solid red')
-  );
+  if (!lastX.test(last.value)) {
+    last.style.border = 'solid red';
+    isValid = false;
+    var lastAlert = document.createElement('p');
+    lastAlert.setAttribute('id', 'lastAlert');
+    var lastLabel = document.querySelector('label[for="last"]');
+    lastAlert.textContent = errorMessages['last'];
+    if (lastLabel.firstElementChild == null) {
+      lastLabel.appendChild(lastAlert);
+    }
+  } else {
+    last.style.border = 'solid green';
+    if (document.contains(document.getElementById('lastAlert'))) {
+      document.getElementById('lastAlert').remove();
+    }
+  }
 }
 
 function validateEmail() {
-  emailX.test(
-    email.value
-      ? (email.style.border = 'solid green')
-      : (email.style.border = 'solid red')
-  );
+  if (!emailX.test(email.value)) {
+    email.style.border = 'solid red';
+    isValid = false;
+    var emailAlert = document.createElement('p');
+    emailAlert.setAttribute('id', 'emailAlert');
+    var emailLabel = document.querySelector('label[for="email"]');
+    emailAlert.textContent = errorMessages['email'];
+    if (emailLabel.firstElementChild == null) {
+      emailLabel.appendChild(emailAlert);
+    }
+  } else {
+    email.style.border = 'solid green';
+    if (document.contains(document.getElementById('emailAlert'))) {
+      document.getElementById('emailAlert').remove();
+    }
+  }
 }
 
 function validateQuantity() {
-  quantityX.test(
-    quantity.value
-      ? (quantity.style.border = 'solid green')
-      : (quantity.style.border = 'solid red')
-  );
+  if (!quantityX.test(quantity.value)) {
+    quantity.style.border = 'solid red';
+    isValid = false;
+    var quantityAlert = document.createElement('p');
+    quantityAlert.setAttribute('id', 'quantityAlert');
+    var quantityLabel = document.querySelector('label[for="quantity"]');
+    quantityAlert.textContent = errorMessages['quantity'];
+    if (quantityLabel.firstElementChild == null) {
+      quantityLabel.appendChild(quantityAlert);
+    }
+  } else {
+    quantity.style.border = 'solid green';
+    if (document.contains(document.getElementById('quantityAlert'))) {
+      document.getElementById('quantityAlert').remove();
+    }
+  }
 }
 
 function validateBirthDate() {
-  birthdateX.test(
-    birthdate.value
-      ? (birthdate.style.border = 'solid green')
-      : (birthdate.style.border = 'solid red')
-  );
+  if (
+    isNaN(Date.parse(birthdate.value)) ||
+    Date.parse(birthdate.value) < -2079302400000 ||
+    Date.parse(birthdate.value) > Date.parse(Date())
+  ) {
+    birthdate.style.border = 'solid red';
+    isValid = false;
+    var birthdateAlert = document.createElement('p');
+    birthdateAlert.setAttribute('id', 'birthdateAlert');
+    var birthdateLabel = document.querySelector('label[for="birthdate"]');
+    birthdateAlert.textContent = errorMessages['birthdate'];
+    if (birthdateLabel.firstElementChild == null) {
+      birthdateLabel.appendChild(birthdateAlert);
+    }
+  } else {
+    birthdate.style.border = 'solid green';
+    if (document.contains(document.getElementById('birthdateAlert'))) {
+      document.getElementById('birthdateAlert').remove();
+    }
+  }
 }
 
 function validateRadio() {
   for (let i = 0; i < radios.length; ++i) {
-    if (radios[i].checked == true) return true;
+    if (radios[i].checked == true) return greenRadio();
   }
-  return false;
+  return redRadio();
+}
+
+function redRadio() {
+  let radioIcon = document.querySelectorAll('.checkbox-label .checkbox-icon ');
+  for (let i = 0; i < radioIcon.length; i++) {
+    radioIcon[i].style.border = 'solid red';
+  }
+  isValid = false;
+  var radiosAlert = document.createElement('p');
+  radiosAlert.setAttribute('id', 'radiosAlert');
+  var radiosLabel = document.querySelector('.text-label');
+  radiosAlert.textContent = errorMessages['location'];
+  if (!document.contains(document.getElementById('radiosAlert'))) {
+    radiosLabel.append(radiosAlert);
+  }
+}
+
+function greenRadio() {
+  let radioIcon = document.querySelectorAll('.checkbox-label .checkbox-icon ');
+  for (let i = 0; i < radioIcon.length; i++) {
+    radioIcon[i].style.border = 'solid green';
+  }
+  if (document.contains(document.getElementById('radiosAlert'))) {
+    document.getElementById('radiosAlert').remove();
+  }
 }
 
 function validateCG() {
-  return cg.checked;
+  if (!cg.checked) {
+    var cgIcon = document.querySelector('.checkbox2-label .checkbox-icon ');
+    cgIcon.style.border = 'solid red';
+    isValid = false;
+    var cgAlert = document.createElement('p');
+    cgAlert.setAttribute('id', 'cgAlert');
+    var cgLabel = document.querySelector('label[for="checkbox1"]');
+    cgAlert.textContent = errorMessages['checkbox'];
+    if (!document.contains(document.getElementById('cgAlert'))) {
+      cgLabel.parentNode.insertBefore(
+        cgAlert,
+        document.getElementById(checkbox1)
+      );
+    }
+  } else {
+    var cgIcon = document.querySelector('.checkbox2-label .checkbox-icon ');
+    cgIcon.style.border = 'solid green';
+    if (document.contains(document.getElementById('cgAlert'))) {
+      document.getElementById('cgAlert').remove();
+    }
+  }
 }
 
 function validateAll() {
-  for (let i = 0; i < inputs.length; ++i) {
-    const msgError = errorMessages[input];
-    eval(inputs[i] + 'X').test(inputs[i].value);
-  }
-
-  (input) => validateFirst();
+  validateFirst();
   validateLast();
   validateEmail();
-  validateQuanlety();
+  validateQuantity();
   validateBirthDate();
   validateRadio();
   validateCG();
 }
 
 // MESSAGES
-const errorMessages = {
+var errorMessages = {
   first: 'Veuillez entrer un nom comportant 2 caractères ou plus.',
   last: 'Veuillez entrer un prénom comportant 2 caractères ou plus.',
   email: 'Veuillez entrer une adresse email valide.',
@@ -156,32 +255,32 @@ const inputs = [
   'cg',
 ];
 
-//CLASS SAMPLE
+// //CLASS SAMPLE
 
-class minLength {
-  constructor(minLength) {
-    this.minLength = minLength;
-  }
-  validate(value) {
-    return value.length > this.minLength;
-  }
-}
-const validationRules = {
-  '#first': [
-    new minLength(2),
-    new maxLength(30),
-    new matchRegex(/^[A-Z]{1}[aZ-zZ]*$/),
-  ],
-};
-function validate() {
-  for (const [key, rules] of Object.entries(validationRules)) {
-    const element = document.querySelector(key);
-    rules.forEach((rule) => {
-      if (rule.validate(element)) {
-        // Pas d'erreur
-      } else {
-        // Erreur
-      }
-    });
-  }
-}
+// class minLength {
+//   constructor(minLength) {
+//     this.minLength = minLength;
+//   }
+//   validate(value) {
+//     return value.length > this.minLength;
+//   }
+// }
+// const validationRules = {
+//   '#first': [
+//     new minLength(2),
+//     new maxLength(30),
+//     new matchRegex(/^[A-Z]{1}[aZ-zZ]*$/),
+//   ],
+// };
+// function validate() {
+//   for (const [key, rules] of Object.entries(validationRules)) {
+//     const element = document.querySelector(key);
+//     rules.forEach((rule) => {
+//       if (rule.validate(element)) {
+//         // Pas d'erreur
+//       } else {
+//         // Erreur
+//       }
+//     });
+//   }
+// }
